79160da1f3d7a18a5f32264c989f1354878983da
Jean Paul Delimat <jpd@netcompany.com>
2019-10-22 08:44:32 +0200

    Lab1Ex4

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -5,12 +5,15 @@ const generateArray = (n) => [...Array(10).keys()].map(x => x+1);
 
 const generateRandomArray = (n) => Array(n).fill().map(() => Math.floor(Math.random() * 25) + 1);
 
+const array1 = [2, 56, 23, 88, 17, 4];
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <h1>Minimal React delimatj</h1>  
     <p>Array of integers from 1 to 10: {JSON.stringify(generateArray(10))}</p>
     <p>Array of random integers from 1 to 25: {JSON.stringify(generateRandomArray(10))}</p>
+    <p>Array of elements from  {JSON.stringify(array1)} that are larger than 15: {JSON.stringify(array1.filter(x => x > 15))}</p>
   </div>
 )
 
