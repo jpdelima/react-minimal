bba28e63043debf3722b33b9e2b7ce51397fc0ff
Jean Paul Delimat <jpd@netcompany.com>
2019-10-22 08:45:58 +0200

    Lab1Ex5

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -7,6 +7,10 @@ const generateRandomArray = (n) => Array(n).fill().map(() => Math.floor(Math.ran
 
 const array1 = [2, 56, 23, 88, 17, 4];
 
+const array2 = [2, 5, 8, 10];
+
+const squareRoots = (arr) => arr.map(Math.sqrt);
+
 const MyApp = () => (
   <div>
     <AppTitle />
@@ -14,6 +18,7 @@ const MyApp = () => (
     <p>Array of integers from 1 to 10: {JSON.stringify(generateArray(10))}</p>
     <p>Array of random integers from 1 to 25: {JSON.stringify(generateRandomArray(10))}</p>
     <p>Array of elements from  {JSON.stringify(array1)} that are larger than 15: {JSON.stringify(array1.filter(x => x > 15))}</p>
+    <p>Square roots of {JSON.stringify(array2)}: {JSON.stringify(squareRoots(array2))}</p>
   </div>
 )
 
