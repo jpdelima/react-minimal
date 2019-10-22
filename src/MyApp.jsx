53fe2f4ecdae3e6b46f0ac3d4c5a0b15b32bf7ef
Jean Paul Delimat <jpd@netcompany.com>
2019-10-22 08:43:08 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,11 +3,14 @@ import AppTitle from './AppTitle'
 
 const generateArray = (n) => [...Array(10).keys()].map(x => x+1);
 
+const generateRandomArray = (n) => Array(n).fill().map(() => Math.floor(Math.random() * 25) + 1);
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <h1>Minimal React delimatj</h1>  
     <p>Array of integers from 1 to 10: {JSON.stringify(generateArray(10))}</p>
+    <p>Array of random integers from 1 to 25: {JSON.stringify(generateRandomArray(10))}</p>
   </div>
 )
 
