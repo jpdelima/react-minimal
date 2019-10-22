8be490269243f75941053568564569406c0e93cb
Jean Paul Delimat <jpd@netcompany.com>
2019-10-22 08:40:11 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import AppTitle from './AppTitle'
 
+const generateArray = (n) => [...Array(10).keys()].map(x => x+1);
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <h1>Minimal React delimatj</h1>  
+    <p>Array of integers from 1 to 10: {JSON.stringify(generateArray(10))}</p>
   </div>
 )
 
